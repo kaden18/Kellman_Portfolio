09d7c04fd0df8b9f19a65183c777e19c1df44e1c
kaden18 <kbrimank@yahoo.com>
2017-05-30 15:42:46 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function myFunction()
   {
       document.getElementById('container').style.backgroundColor = 'rgba(255, 255, 255, .9)';
       document.body.style.color = '#000000';
-      document.getElementById('nav').style.backgroundColor = 'rgba(0, 0, 0, .75)';
+      document.getElementById('nav').style.backgroundColor = 'rgba(0, 0, 0, .1)';
       document.getElementById('email').style.color = 'rgb(0, 0, 0)';
       document.getElementById('mayLink').style.color = 'rgb(0, 0, 0)';
   }
