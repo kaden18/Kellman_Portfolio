86a6948ab5004965d73616b847669c7e41b7c655
kaden18 <kbrimank@yahoo.com>
2017-05-30 15:44:59 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function myFunction()
 
   if (n < 18 && n >= 6)
   {
-      document.getElementById('container').style.backgroundColor = 'rgba(255, 255, 255, .9)';
+      document.getElementById('container').style.backgroundColor = 'rgba(255, 255, 255, .95)';
       document.body.style.color = '#000000';
       document.getElementById('nav').style.backgroundColor = 'rgba(0, 0, 0, .95)';
       document.getElementById('email').style.color = 'rgb(0, 0, 0)';
