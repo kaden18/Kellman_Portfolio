184eff186b77e3038bbc1becf6cd53138ee4be50
kaden18 <kbrimank@yahoo.com>
2017-05-30 11:22:30 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function myFunction()
+{
+  var d = new Date();
+  var n = d.getHours();
+
+  if (n < 18 && n >= 6)
+  {
+      document.getElementById('container').style.backgroundColor = 'rgba(255, 255, 255, .9)';
+      document.body.style.color = '#000000';
+      document.getElementById('nav').style.backgroundColor = 'rgba(0, 0, 0, .75)';
+      document.getElementById('email').style.color = 'rgb(0, 0, 0)';
+  }
+  else {}
+}
+
+myFunction();
